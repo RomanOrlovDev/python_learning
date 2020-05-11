4fcfdc3802d0d924341227ed3b65d9ef36ece793
rorlov <r.orlov@itgrco.com>
2020-05-11 23:49:33 +0300

    guides changed

--- guide_sets.py
+++ guide_sets.py
@@ -1,4 +1,6 @@
 import random
+# also called tuple
+# is immutable
 
 random_set = set()
 while True:
@@ -11,3 +13,9 @@ while True:
 print(len(random_set) + 1)
 print(random_set)
 print(random_value)
+
+
+some_set = (2, 3, 5)
+print(some_set[1])
+some_set[1] = 23
+print(some_set)
